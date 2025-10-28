1ad3d241fe6113eda8fd08948a9df585fd3a6ac9
Yuri Galvao <ygalvao514@gmail.com>
2025-10-27 21:03:09 -0300

    Estudos do dia 27/10

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.blog, name='home'),
+    path('<int:post_id>', views.post, name='post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from blog.data import posts
+from typing import Any
+from django.http import Http404
 
 # Create your views here.
 
 def blog(request):
     context = {
-        'text': 'Ola Blog',
-        'tittle': 'Pagina Exemplo - ',
+        #'text': 'Ola Blog',
+        'posts': posts
     }
     return render(request, 'blog/index.html', context)
+
+def post(request, post_id):
+    found_post: dict[str, Any] | None = None
+
+    for post in posts:
+        if post['id'] == post_id:
+            found_post = post
+            break
+
+        if found_post is None:
+            raise Http404('Post nao existe')
+
+    context = {
+        'post': found_post,
+        'title':found_post['title'] + ' - ',
+    }
+    return render(request, 'blog/post.html', context)
